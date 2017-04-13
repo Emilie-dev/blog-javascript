aa1afb68cda36a83b23ec06edb9460622af796cf
Chauvin <chauvin-e@orange.fr>
2017-04-13 17:27:00 +0200

    Suite de l'exercice

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-var  = {name:"emilie"}
+var blog = [];
+var titre = "";
+var texte = "";
+
+$("#titre").keyup(function(){
+	titre = $("#titre").val();
+})
+
+$("#texte").keyup(function() {
+	texte = $("#texte").val();
+})
+
+var contenu = {"titre" : titre, "texte" : texte}
+$("#titre").val();
+$("#texte").val();
+blog.push(contenu);
 
 $.ajax({
 	url : "http://192.168.1.50/json-db"
 	data : {
 		task : "set",
 		key : "blogemilie",
-		value : JSON.stringify(),
+		value : JSON.stringify(blog),
 	},
    
 })
-
-
 
 	$.ajax({
 		url :"http://192.168.1.50/json-db",
