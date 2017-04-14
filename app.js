38583b02909e90c6cfa694fb96c3ce7f46610a4a
Chauvin <chauvin-e@orange.fr>
2017-04-14 14:47:52 +0200

    Ajax fonctionnel

--- app.js
+++ app.js
@@ -1,46 +1,68 @@
 var blog = [];
-var titre = "";
-var texte = "";
 
-$("#titre").keyup(function(){
-	titre = $("#titre").val();
-})
 
-$("#texte").keyup(function() {
-	texte = $("#texte").val();
-})
 
-var contenu = {"titre" : titre, "texte" : texte}
-$("#titre").val("");
-$("#texte").val("");
-blog.push(contenu);
 
-$.ajax({
-	url : "http://192.168.1.50/json-db"
-	data : {
-		task : "set",
-		key : "blogemilie",
-		value : JSON.stringify(blog),
-	},
-   
-})
+function afficher(data) {
+	console.log(data);
+	$("#public").html(data);	
+}
+
+
+
+
+
+$("#envoyer").click(function(){
+	
+	var contenu = {
+			"titre" : $("#titre").val(),
+			"texte" : $("#texte").val(),
+		};
+	
+	blog.push(contenu);
+	sauvegarde();
+	$("#titre").val("");
+	$("#texte").val("");
+});
+
+
+// j'envoie au serveur
+
+function sauvegarde() {
 
 	$.ajax({
-		url :"http://192.168.1.50/json-db",
+		url : "http://192.168.1.50/json-db",
 		data : {
-		task : "get",
-		key : "blogemilie",
-		}
-	})
-	.done(function() {
-		alert("done");
-	})
-	.fail(function() {
-		alert("error");
-	})
-	.always(function() {
-		alert("complete");
-	});
+			task : "set",
+			key : "blogemilie",
+			value : JSON.stringify(blog)
+		},
    
+	})
+}
+
+
+// je récupére depuis le serveur
+
+$.ajax({
+	url :"http://192.168.1.50/json-db",
+	data : {
+		task : "get",
+		key : "blogemilie"
+	}
+})
+.done(function(data) {
+//	alert("done");
+	afficher(data);
+})/*
+.fail(function() {
+	alert("error");
+})
+.always(function() {
+	alert("complete");
+});*/
+
+
+
 
 
